4f84675c4566599cc1bcc0893c2498597a84098f
Elijah Prince <elijahprince73@gmail.com>
2018-02-12 22:19:11 -0800

    Getting individual todo

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {
+	ObjectID
+} = require('mongodb');
+
+const {
 	mongoose
 } = require('./db/mongoose');
 const {
@@ -27,6 +31,8 @@ app.post('/todos', (req, res) => {
 	})
 })
 
+
+// Gets all todos
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({
@@ -34,6 +40,24 @@ app.get('/todos', (req, res) => {
 		})
 	}, (e) => {
 		res.status(400).send(e)
+	})
+})
+
+// Gets single todo
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('Invalid Id')
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send({})
+		}
+		res.send({
+			todo
+		})
+	}).catch((err) => {
+		res.status(404).send('Unable to find that todo')
 	})
 })
 
