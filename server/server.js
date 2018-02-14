0b506c163e803b634e409592e60f852cae5a95b8
Elijah Prince <elijahprince73@gmail.com>
2018-02-13 22:37:24 -0800

    added enviroment variables

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+// Heroku
+const env = process.env.NODE_ENV || 'development'
+console.log('env *********', env);
+if (env === 'development') {
+	process.env.PORT = 4000
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+} else if (env === 'test') {
+	process.env.PORT = 4000
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -17,7 +28,8 @@ const {
 } = require('./models/todo.js');
 
 const app = express()
-const port = process.env.PORT || 4000
+
+const port = process.env.PORT
 
 app.use(bodyParser.json())
 
